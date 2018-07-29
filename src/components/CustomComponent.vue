1820f3340412d07de7e66bd98ee7daa2a9886ebb
Lawrence <lawbraun.almeida@gmail.com>
2018-07-29 17:54:07 +0100

    initial commit

---
+++ src/components/CustomComponent.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="custom-component"
+    :style="{
+      'background-color': backgroundColor,
+      'border-color': borderColor,
+      'border-style': 'solid',
+      'border-width': '2px',
+      'border-radius': '4px',
+      'padding': '15px',
+      'width': '300px',
+      'margin': '0 auto'
+    }"
+  >
+    <input v-model="name" type="text" placeholder="Name" class="input" @blur="showValue">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CustomComponent',
+  data () {
+    return {
+      name: '',
+      backgroundColor: '#EEE',
+      borderColor: '#DDD'
+    }
+  },
+  mounted(){
+    let q = this.$router.currentRoute.query
+    this.backgroundColor = q.backgroundColor
+    this.borderColor = q.borderColor
+  },
+  methods:{
+    showValue () {
+      alert(this.name)
+    }
+  }  
+}
+</script>
+
+<style>
+  .input{
+    width: 100%;
+    padding: 2px 4px;
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,28 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+import CustomComponent from './components/CustomComponent.vue'
+
 import App from './App.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    name: 'home',
+    path: '/',
+    component: CustomComponent,
+    props: true
+  }
+]
 
 Vue.config.productionTip = false
 
+const router = new VueRouter({
+  mode: 'history',
+  routes: routes
+})
+
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
